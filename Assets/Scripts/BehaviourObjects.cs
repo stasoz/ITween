f9bd694f5f415eea60231aa1c88935b60f05a824
stasoz <truestasoz@gmail.com>
2018-04-17 11:02:48 -0700

    v.1

---
+++ Assets/Scripts/BehaviourObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourObjects : MonoBehaviour
+{
+    public GameObject[] objs;
+    public GameObject[] points;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) ChangeObjects();
+    }
+    void ChangeObjects()
+    {
+        iTween.ColorTo(objs[0], iTween.Hash("color", Color.red,"time",2));
+        iTween.MoveTo(objs[0], iTween.Hash("position", points[0].transform.position, "time", 4,"easytype",iTween.EaseType.easeInOutElastic));
+        iTween.RotateFrom(objs[1], iTween.Hash("y", 90.0f, "time", 3, "easetype", iTween.EaseType.easeInExpo, "looktarget",objs[0]));
+        iTween.ScaleTo(objs[1],new Vector3(2,2,2),3);
+        iTween.ShakeRotation(objs[2],new Vector3(10,3,3),2f);
+        iTween.ShakePosition(Camera.main.gameObject, iTween.Hash("x",0.7f,"y", 0.3f, "time", 0.8f, "delay", 2.0f));
+    }
+}
