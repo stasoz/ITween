0d6f7b7276b8e3f6dd0b577fbdbbf702735e1c93
stasoz <truestasoz@gmail.com>
2018-04-17 12:54:36 -0700

    v.2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static bool goCube=false;
+    public static bool goSphere = false;
+    public static bool goCylinder = false;
+    public static bool goWall = false;
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 250, 150), "<color=black><size=20>Click on the object and on the back wall</size></color>");
+    }
+    public void RestartLevel()
+    {
+        Application.LoadLevel(0);
+    }
+}
