03b9e8862985f9e3b694da055022e029310f28da
Stas Zgurskiy <truestasoz@gmail.com>
2018-04-18 15:13:17 -0700

    Add files via upload

---
+++ Assets/Scripts/ScrpitForRememberColor/AnimationPrefabs.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPrefabs : MonoBehaviour {
+    public void OnMouseDown()
+    {
+        iTween.MoveFrom(this.gameObject, new Vector3(transform.position.x,-2,transform.position.z),0.5f);
+    }
+}
---
+++ Assets/Scripts/ScrpitForRememberColor/StateOfColors.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateOfColors : MonoBehaviour
+{
+    public GameObject[] objs;
+    private int[] randomNumbers;
+    private enum Colors:int
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow
+    }
+    private void Start()
+    {
+        SetUpColor();
+    }
+    void SetUpColor()
+    {
+        foreach(GameObject g in objs)
+        {
+            switch((Colors)Random.Range(0, 4))
+            {
+                case Colors.Red:
+                    g.GetComponent<MeshRenderer>().material.color = Color.red;
+                    break;
+                case Colors.Green:
+                    g.GetComponent<MeshRenderer>().material.color = Color.green;
+                    break;
+                case Colors.Blue:
+                    g.GetComponent<MeshRenderer>().material.color = Color.blue;
+                    break;
+                case Colors.Yellow:
+                    g.GetComponent<MeshRenderer>().material.color = Color.yellow;
+                    break;
+            }
+            
+        }
+    }
+}
